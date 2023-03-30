75b369d5719abd73b9569ef876e9fdfc300e7596
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-03-30 19:40:04 +0000

    initializeClock

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-function myFunction() {
-//alert('hello world!');
-  document.getElementById("demo").innerHTML = "Playing around with JS";
+const deadline = '2023-03-30'
+
+alert(deadline);
+
+function getTimeRemaining(endtime){
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total/1000)%60);
+  const minutes = Math.floor((total/1000/60)$60);
+  const hours = Math.floor((total/(1000*60*60))%24);
+  cosnt days = Math.floor(total/(1000*60*60*24));
+
+  return{total, days, hour, minutes seconds}
 }
+
+function initializeClock(id,endtime){
+  const clock = document.getElementByID(id);
+  const timeinterval = setInterval(() =>
+    {const t = getTimerRemaining(endtime);
+    clock.innerHTML = 'days: ' + t.days +'<br>'  +
+                      'hours: '+t.hours +'<br>'  +
+                      'mins:  '+t.minutes+'<br>' +
+                      'seconds:'+t.seconds+'<br>'+
+    if (t.total <= 0)
+    {
+      clearInterval(timeinterval);
+    }
+    },1000);
+}
+
+initializedClock('clockdiv', deadline);
